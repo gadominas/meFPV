40c1a7d7754091d837aa2b2343350339f4de88a3
gsiutilas <gadominas@gmail.com>
2023-02-11 23:21:14 +0200

    Revert "initial seed"
    This reverts commit 02ba9982223452a2b9ed20f5c09fa3bc5cf2704e.

---
+++ mock/resources/layers/page858399277-plain.js
@@ -0,0 +1,124 @@
+rabbit.data.layerStore.addLayerFromHtml('<div xmlns:sketchedHelper="java:it.rapidrabb.editor.stencils.helpers.SketchedHelper" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:fn="http://www.w3.org/2005/xpath-functions">\
+   <div id="result">\
+      <div xmlns:xs="http://www.w3.org/2001/XMLSchema" id="__containerId__page858399277-layer" class="layer" name="__containerId__pageLayer" data-layer-id="page858399277" data-layer-type="pageLayer" style="position:absolute;left:0px;top:0px;">\
+         <div id="__containerId__-page858399277-layer-text31510040" style="position: absolute; left: 0px; top: 0px; width: 1366px; height: 660px;-webkit-transform: rotate(0deg);transform: rotate(0deg);" data-stencil-type="default.text3" data-interactive-element-type="default.text3" class="text3 stencil mobile-interaction-potential-trigger p-DefaultText3" data-stencil-id="text31510040" data-review-reference-id="text31510040">\
+            <div class="stencil-wrapper" style="width: 1366px; height: 660px">\
+               <svg xmlns="http://www.w3.org/1999/xhtml" class="p-DefaultText3--BackgroundContainer" overflow="hidden" style="height: 660px;width:1366px;" width="1366" height="660">\
+                  <path d="m 0.0,0 l 1366.0,0 a 0.0,0.0 0 0 1 0.0,0.0 l 0,660.0 a 0.0,0.0 0 0 1 -0.0,0.0l -1366.0 0 a 0.0,0.0 0 0 1 -0.0,-0.0 l 0,-660.0 a 0.0,0.0 0 0 1 0.0,-0.0" style=" fill: #F7F4F4; stroke: rgba(0, 0, 0, 1); stroke-width: 1;"></path>\
+               </svg>\
+               <div xmlns="http://www.w3.org/1999/xhtml" title="" class="p-DefaultText3--CenterParent" style="width:1352px;height:646px;left: 0px;"><span class="u-p-verticalAlignMiddle p-DefaultText3--CenterChild u-p-textCenter" data-child-type="static.rect2"><span class="p-DefaultText3--Container" data-child-type="static.rect2">\
+                        <p style="width: 100%;font-size: 14px;text-align: center;color: #000000;"></p></span></span></div>\
+               <svg xmlns="http://www.w3.org/1999/xhtml" class="p-DefaultText3--DisableOverlayContainer u-p-hidden" overflow="hidden" style="height: 660px;width:1366px;" width="1366" height="660">\
+                  <path class="p-DefaultText3--DisableOverlay" d="m 0.0,0 l 1366.0,0 a 0.0,0.0 0 0 1 0.0,0.0 l 0,660.0 a 0.0,0.0 0 0 1 -0.0,0.0l -1366.0 0 a 0.0,0.0 0 0 1 -0.0,-0.0 l 0,-660.0 a 0.0,0.0 0 0 1 0.0,-0.0" style="stroke-width: 1;"></path>\
+               </svg>\
+            </div>\
+         </div>\
+         <div id="__containerId__-page858399277-layer-textinput24168302" style="position: absolute; left: 475px; top: 295px; width: 300px; height: 30px;-webkit-transform: rotate(0deg);transform: rotate(0deg);" data-stencil-type="default.textinput" data-interactive-element-type="default.textinput" class="textinput pidoco-clickable-element stencil mobile-interaction-potential-trigger " data-stencil-id="textinput24168302" data-review-reference-id="textinput24168302">\
+            <div class="stencil-wrapper" style="width: 300px; height: 30px">\
+               <div xmlns="http://www.w3.org/1999/xhtml" title=""><input type="text" id="__containerId__-page858399277-layer-textinput24168302input" value="Lithuania: bando -forest +house" style="width:298px;height:28px;padding: 0px;border-width:1px;border-color:rgba(0, 0, 0, 1); background-color:rgba(255, 255, 255, 1); color: rgba(0, 0, 0, 1);" class="" onsubmit="debugger" /></div>\
+            </div>\
+            <div class="interactive-stencil-highlighter" style="width: 300px; height: 30px"></div>\
+            <div xmlns:json="http://json.org/" xmlns="http://www.w3.org/1999/xhtml"><script type="text/javascript">\
+				$(document).ready(function(){\
+					rabbit.errorContext(function () {\
+						rabbit.interaction.manager.registerInteraction(\'__containerId__-page858399277-layer-textinput24168302\', \'interaction415584409\', {"button":"left","id":"action485894397","numberOfFinger":"1","type":"click"},  \
+							[\
+								{"delay":"0","id":"reaction264289795","options":"withoutReloadOnly","target":"page574898694","transition":"none","type":"showPage"}\
+							]\
+						);\
+					});\
+				});\
+			</script></div>\
+         </div>\
+         <div id="__containerId__-page858399277-layer-text344813633" style="position: absolute; left: 785px; top: 295px; width: 67px; height: 30px;-webkit-transform: rotate(0deg);transform: rotate(0deg);" data-stencil-type="default.text3" data-interactive-element-type="default.text3" class="text3 pidoco-clickable-element stencil mobile-interaction-potential-trigger p-DefaultText3" data-stencil-id="text344813633" data-review-reference-id="text344813633">\
+            <div class="stencil-wrapper" style="width: 67px; height: 30px">\
+               <svg xmlns="http://www.w3.org/1999/xhtml" class="p-DefaultText3--BackgroundContainer" overflow="hidden" style="height: 30.0px;width:67.0px;" width="67.0" height="30.0">\
+                  <path d="m 5.0,0 l 57.0,0 a 5.0,5.0 0 0 1 5.0,5.0 l 0,20.0 a 5.0,5.0 0 0 1 -5.0,5.0l -57.0 0 a 5.0,5.0 0 0 1 -5.0,-5.0 l 0,-20.0 a 5.0,5.0 0 0 1 5.0,-5.0" style=" fill: rgba(217, 217, 217, 1); stroke: rgba(0, 0, 0, 1); stroke-width: 1;"></path>\
+               </svg>\
+               <div xmlns="http://www.w3.org/1999/xhtml" title="" class="p-DefaultText3--CenterParent" style="width:53px;height:16px;left: 0px;"><span class="u-p-verticalAlignMiddle p-DefaultText3--CenterChild u-p-textCenter" data-child-type="default.button2"><span class="p-DefaultText3--Container" data-child-type="default.button2">\
+                        <p style="width: 100%; font-size: 14px; text-align: center; color: #000000;">fpv</p></span></span></div>\
+               <svg xmlns="http://www.w3.org/1999/xhtml" class="p-DefaultText3--DisableOverlayContainer u-p-hidden" overflow="hidden" style="height: 30.0px;width:67.0px;" width="67.0" height="30.0">\
+                  <path class="p-DefaultText3--DisableOverlay" d="m 5.0,0 l 57.0,0 a 5.0,5.0 0 0 1 5.0,5.0 l 0,20.0 a 5.0,5.0 0 0 1 -5.0,5.0l -57.0 0 a 5.0,5.0 0 0 1 -5.0,-5.0 l 0,-20.0 a 5.0,5.0 0 0 1 5.0,-5.0" style="stroke-width: 1;"></path>\
+               </svg>\
+            </div>\
+            <div class="interactive-stencil-highlighter" style="width: 67px; height: 30px"></div>\
+            <div xmlns:json="http://json.org/" xmlns="http://www.w3.org/1999/xhtml"><script type="text/javascript">\
+				$(document).ready(function(){\
+					rabbit.errorContext(function () {\
+						rabbit.interaction.manager.registerInteraction(\'__containerId__-page858399277-layer-text344813633\', \'interaction569723541\', {"button":"left","id":"action401023821","numberOfFinger":"1","type":"click"},  \
+							[\
+								{"delay":"0","id":"reaction364294791","options":"withoutReloadOnly","target":"page574898694","transition":"none","type":"showPage"}\
+							]\
+						);\
+					});\
+				});\
+			</script></div>\
+         </div>\
+         <div id="__containerId__-page858399277-layer-image360428618" style="position: absolute; left: 580px; top: 135px; width: 145px; height: 145px;-webkit-transform: rotate(0deg);transform: rotate(0deg);" data-stencil-type="default.image" data-interactive-element-type="default.image" class="image stencil mobile-interaction-potential-trigger " data-stencil-id="image360428618" data-review-reference-id="image360428618">\
+            <div class="stencil-wrapper" style="width: 145px; height: 145px">\
+               <div xmlns="http://www.w3.org/1999/xhtml" xmlns:pidoco="http://www.pidoco.com/util" title="">\
+                  <svg class="image-cropper" style="height: 145.0px;width:145.0px;" width="145.0" height="145.0" viewBox="0 0 145.0 145.0">\
+                     <g width="145.0" height="145.0">\
+                        <svg x="0" y="0" width="145.0" height="145.0">\
+                           <image xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="650" height="650" xlink:href="../repoimages/fpv-boy-black-design-clear-bg-for-teams-pilots-quadcopter-fpv-racing.jpg" preserveAspectRatio="none" transform="scale(0.2230769230769231,0.2230769230769231) translate(-0,-0)  " onerror="rabbit.stencils.image.displayPlaceholder(this)"></image>\
+                           <g class="notFoundImagePlaceholder" style="display: none;">\
+                              <rect x="0" y="0" width="145.0" height="145.0" style="stroke:black; stroke-width:1;fill:white;"></rect>\
+                              <line x1="0" y1="0" x2="145.0" y2="145.0" style="stroke:black; stroke-width:0.5;"></line>\
+                              <line x1="0" y1="145.0" x2="145.0" y2="0" style="stroke:black; stroke-width:0.5;"></line>\
+                           </g>\
+                        </svg>\
+                     </g>\
+                  </svg>\
+               </div>\
+            </div>\
+         </div>\
+      </div>\
+   </div>\
+   <div id="styles">\
+      <style type="text/css">\
+         		\
+         		.repository[data-review-reference-id="page858399277"], .transition-wrapper[data-page-id="page858399277"] .layer-container\
+         {\
+         			position: absolute;\
+         			background-color: rgba(255, 255, 255, 1);\
+         		}\
+         	\
+         		body[data-current-page-id="page858399277"] .border-wrapper,\
+         		body[data-current-page-id="page858399277"] .simulation-container {\
+         			width:1366px;\
+         		}\
+         		\
+         		body.has-frame[data-current-page-id="page858399277"] .border-wrapper,\
+         		body.has-frame[data-current-page-id="page858399277"] .simulation-container {\
+         			height:660px;\
+         		}\
+         		\
+         		body[data-current-page-id="page858399277"] .svg-border-1366-660 {\
+         			display: block !important;\
+         		}\
+         		\
+         		body[data-current-page-id="page858399277"] .border-wrapper .border-div {\
+         			width:1366px;\
+         			height:660px;\
+         		}\
+         	\
+      </style>\
+   </div>\
+   <div id="json">\
+      		{\
+      			"id": "page858399277",\
+      			"name": "index",\
+      			"layers": {\
+      				\
+      			},\
+      			"image":"../resources/icons/no_image.png",\
+      			"width":1366,\
+      			"height":660,\
+      			"parentFolder": "",\
+      			"frame": "browser",\
+      			"frameOrientation": "landscape",\
+      			"renderAboveLayer": ""\
+      		}\
+      	\
+   </div>\
+</div>');
